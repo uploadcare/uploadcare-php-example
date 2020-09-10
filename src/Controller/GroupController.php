99675ab4dbdebf1c10246619066571a968571e4a
Andrew Zhdanovskih <azh@crtweb.ru>
2020-09-10 09:40:31 +0500

    Group and group files info

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types=1);
 
-
 namespace App\Controller;
-
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,6 +18,7 @@ class GroupController extends AbstractController
 
     /**
      * @Route(path="/groups", name="groups_list")
+     *
      * @return Response
      */
     public function index(): Response
@@ -31,15 +30,33 @@ class GroupController extends AbstractController
 
     /**
      * @Route(path="/group-info/{uuid<.+>?}", name="group_info")
+     *
      * @param string $uuid
+     *
      * @return Response
      */
     public function info(string $uuid): Response
     {
         $group = $this->api->group()->groupInfo($uuid);
+        $filesInfo = $this->api->uploader()->groupInfo($uuid);
 
         return $this->render('groups/info.html.twig', [
             'group' => $group,
+            'files' => $filesInfo,
         ]);
+    }
+
+    /**
+     * @Route(path="/group-store/{uuid<.+>?}", name="group_store")
+     *
+     * @param string $uuid
+     *
+     * @return Response
+     */
+    public function storeGroup(string $uuid): Response
+    {
+        $this->api->group()->storeGroup($uuid);
+
+        return $this->redirectToRoute('group_info', ['uuid' => $uuid]);
     }
 }
